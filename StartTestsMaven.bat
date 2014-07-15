a707833467cde55c62e751ff45e8d08b358d2a77
vavramchuk <vavramchuk@salsalabs.com>
2014-07-15 14:42:05 +0300

    1

--- StartTestsMaven.bat
+++ StartTestsMaven.bat
@@ -4,7 +4,7 @@ set TESTNGFILE=RunTests.xml
 REM move to the project folder
 cd /d %javaTestProjectPath%
 REM set path to dir that contains javac.exe and java.exe
-REM set path=C:\Program Files\Java\jdk1.7.0_11\bin
 @echo off
-set /p n= What type of content, do you wont to post? 1 - Link or 2 - Photo:
-mvn -f pom%n%.xml test
+set /p n= What server do you want to test? DEV or TEST:
+set /p s= Where do you want run test? LOCAL or REMOTE:
+mvn -f pom.xml test -Denvironment=%n% -Dserver=%s%
