1144ba8eb50d44d869d7cfea105019889c73809e
vavramchuk <vavramchuk@salsalabs.com>
2014-11-03 18:14:18 +0200

    Object for supporter data

---
+++ src/objects/Supporter.java
@@ -0,0 +1,20 @@
+package objects;
+
+
+public class Supporter {
+	public String cPhone = "32165498765";
+	public String City = "CityT";
+	public String emailDomain="@devnull.test.ignite.net";
+	public String Email = "importedSup";
+	public String Facebook = "FBV";
+	public String First_Name = "Tester";
+	public String Home_Phone = "98765432112";
+	public String Last_Name = "Testerov";
+	public String PreferredLanguage = "English (United States)";
+	public String State = "NY";
+	public String Twitter = "TWV";
+	public String Zip_Code = "65498";
+	public String AddressLine1 = "Street ";
+	public String AddressLine2 = "Street line2 ";
+	public String MiddleName = "MName";
+}
