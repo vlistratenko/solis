8cd21a5cc2b13803a1b39ff1a06802ed29716c82
smatikaynen <smatikaynen@salsalabs.com>
2015-03-27 12:17:02 +0200

    Bug: IG-751. Self documented Selenium Web driver test framework.

---
+++ src/main/java/com/salsalabs/ignite/automation/javadoc/Doclet.java
@@ -0,0 +1,12 @@
+package com.salsalabs.ignite.automation.javadoc;
+
+import com.sun.tools.doclets.standard.Standard;
+
+public class Doclet extends Standard{
+	
+	 public static boolean start(com.sun.javadoc.RootDoc arg0)
+			    {
+		 			return Standard.start(arg0); 
+			    }
+	 
+}
