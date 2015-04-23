62cee7131e2d1260a25adf1c64c530768caa32d2
Olga Vasiliaka <ovasiliaka@salsalabs.com>
2015-04-23 12:00:46 +0300

    Bug: IG-1310. added elements to the petition widget

--- src/main/java/com/salsalabs/ignite/automation/pages/hq/activities/PetitionWidget.java
+++ src/main/java/com/salsalabs/ignite/automation/pages/hq/activities/PetitionWidget.java
@@ -1,11 +1,20 @@
 package com.salsalabs.ignite.automation.pages.hq.activities;
 
 import com.salsalabs.ignite.automation.elements.Button;
+import com.salsalabs.ignite.automation.elements.CheckBox;
+import com.salsalabs.ignite.automation.elements.TextBox;
 import com.salsalabs.ignite.automation.elements.impl.ButtonImpl;
+import com.salsalabs.ignite.automation.elements.impl.CheckBoxImpl;
+import com.salsalabs.ignite.automation.elements.impl.TextBoxImpl;
 
 public class PetitionWidget extends SubscribeWidget {
 	
 	Button signButton = new ButtonImpl("//input[@value='Sign Petition']", "Sign Petition", true);
+	CheckBox fundraisingCheckBox = new CheckBoxImpl("//div[contains(text(), 'Fundraising')]/input", "Fundraising");
+	CheckBox newsletterCheckBox = new CheckBoxImpl("//div[contains(text(), 'Newsletter')]/input", "Newsletter");
+	CheckBox emailCheckBox = new CheckBoxImpl("//label[contains(text(), 'Email')]/preceding-sibling::input", "Email");
+	TextBox comment = new TextBoxImpl("//textarea[@id='field-comment']", "Comment textbox");
+	CheckBox displaySignatureCheckBox = new CheckBoxImpl("//input[name='field-showSignature']", "Display My Signature");
 	
 	public PetitionWidget() {
 		super();
@@ -20,7 +29,8 @@ public class PetitionWidget extends SubscribeWidget {
 		if (visible) {
 			verifier.verifyElementIsDisplayed(true, signButton);
 			signButton.click();
-			verifier.verifyElementIsDisplayed(true, personEmailField, personFNameField, personLNameField, personCityField, personZipField, personStatesSelectBox);
+			verifier.verifyElementIsDisplayed(true, personEmailField, personFNameField, personLNameField, personCityField, personZipField, personStatesSelectBox,
+					fundraisingCheckBox, newsletterCheckBox, emailCheckBox, comment, displaySignatureCheckBox);
 		} else {
 			verifier.verifyElementIsNotDisplayed(true, signButton);
 		}
