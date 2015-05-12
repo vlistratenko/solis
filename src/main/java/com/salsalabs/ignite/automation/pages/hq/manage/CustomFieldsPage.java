4f7611c08d7f5620169c53e2e854d3f810a928d0
Oleksandr Hubachov <ahubachov@salsalabs.com>
2015-05-12 10:43:34 +0300

    Bugzid:IG-875.Scenario: Created custom fields of each type (TC8) - changed autotest-id

--- src/main/java/com/salsalabs/ignite/automation/pages/hq/manage/CustomFieldsPage.java
+++ src/main/java/com/salsalabs/ignite/automation/pages/hq/manage/CustomFieldsPage.java
@@ -16,9 +16,9 @@ public class CustomFieldsPage extends ManagePage {
 	private Button createCFBtn = new ButtonImpl("//*[@id='customFieldsConfiguration']/div/ng-form/div/div/div[2]/div/div/button", "Create a Custom Field", true);
 	private Button toStep2Btn = new ButtonImpl("//*[@autotest-id='btn_next_step1_custom_field_create']", "Continue to Step 2: Field Details »", true);
 	private TextBox fieldNameTxtBox = new TextBoxImpl("//*[@id='cfnew_name']", "Field Name", true);
-	private Button toStep3Btn = new ButtonImpl("//*[@id='btnModalStep2']", "Continue to Step 3: Field Settings »", true);
+	private Button toStep3Btn = new ButtonImpl("//*[@autotest-id='btn_next_step2_custom_field_create']", "Continue to Step 3: Field Settings »", true);
 	private TextBox descriptionTxtBox = new TextBoxImpl("//*[@id='cf_form']/div[2]/div[2]/div[2]/div/textarea", "Description", true);
-	private Button createFieldBtn = new ButtonImpl("//*[@autotest-id='btn_save_step3_custom_field_create']", "Create Field! »", true);
+	private Button createFieldBtn = new ButtonImpl("//*[@autotest-id='custom_field-create_field']", "Create Field! »", true);
 	private TextBox cfValue = new TextBoxImpl("//*[@id='cf_form']/div[2]/div[3]/div[1]/div/div[2]/span/span/input", "Text", true);
 	private Button customFieldButton;
 	
