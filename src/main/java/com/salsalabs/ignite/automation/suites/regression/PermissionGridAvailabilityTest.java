4d14f6bf6df9ade4deb4dadfa83756882b4ade44
Oleg Lauta <olauta@salsalabs.com>
2017-10-06 11:56:58 +0300

    https://jira.salsalabs.net/browse/IG-11437

---
+++ src/main/java/com/salsalabs/ignite/automation/suites/regression/PermissionGridAvailabilityTest.java
@@ -0,0 +1,9 @@
+package com.salsalabs.ignite.automation.suites.regression;
+
+import com.salsalabs.ignite.automation.common.SeleneseTestCase;
+
+/**
+ * Created by bcmbx on 06.10.2017.
+ */
+public class PermissionGridAvailabilityTest extends SeleneseTestCase {
+}
