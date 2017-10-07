3399a7589edd7cb04bd3783e7c83f22b1724f6bf
Vitalii Listratenko <vitalii.listratenko@gmail.com>
2017-10-07 15:34:31 +0300

    eliminated VE elements package issues

--- src/main/java/com/salsalabs/ignite/automation/elements/VE2Elements/VEElements.java
+++ src/main/java/com/salsalabs/ignite/automation/elements/VE2Elements/VEElements.java
@@ -17,7 +17,6 @@ public class VEElements extends ElementImpl {
     public void edit(String fieldName){
         Button fieldEditIcon = new ButtonImpl("//label[.='" + fieldName + "']/ancestor::*[@class='content-render-wrapper']//*[@title='Edit']", fieldName + " edit icon");
         fieldEditIcon.clickJS();
-
     }
 
     public void dragAndDropElementOnLayoutWithFormElement() {
@@ -27,7 +26,7 @@ public class VEElements extends ElementImpl {
         elementsVe.click();
         getLogger().info("Elements menu was clicked");
         WebElement source = findElementByXpath(getElementPath());
-        WebElement target = getDriver().findElement(By.xpath("//div[@class='content-render-wrapper']"));
+        WebElement target = findElementByXpath("//div[@class='content-render-wrapper']");
         action.clickAndHold(source).moveToElement(target).release().perform();
         getLogger().info(getElementName() + " element was dropped into the top of the Visual Editor");
     }
@@ -40,7 +39,7 @@ public class VEElements extends ElementImpl {
         elementsVe.click();
         getLogger().info("Rows menu was clicked");
         WebElement source = findElementByXpath(getElementPath());
-        WebElement target = getDriver().findElement(By.xpath("//*[@class='editorContentWrapper ui-droppable']"));
+        WebElement target = findElementByXpath("//*[@class='editorContentWrapper ui-droppable']");
         action.clickAndHold(source).moveToElement(target).release().perform();
         getLogger().info(getElementName() + " was dropped into the layout");
     }
@@ -53,7 +52,7 @@ public class VEElements extends ElementImpl {
         elementsVe.click();
         getLogger().info("Elements menu was clicked");
         WebElement source = findElementByXpath(getElementPath());
-        WebElement target = getDriver().findElement(By.xpath("//*[@class='render-content ui-droppable']"));
+        WebElement target = findElementByXpath("//*[@class='render-content ui-droppable']");
         action.clickAndHold(source).moveToElement(target).release().perform();
         getLogger().info(getElementName() + " was dropped into the layout");
     }
