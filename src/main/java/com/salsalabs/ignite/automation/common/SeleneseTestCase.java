f64b7c47c55364b4b12b74d1501c7a2ad79d9d54
VitaliiA <VitaliiA@192.168.15.141>
2017-11-21 15:09:22 +0200

    Fixed PermissionGridAvailabilityTest test

--- src/main/java/com/salsalabs/ignite/automation/common/SeleneseTestCase.java
+++ src/main/java/com/salsalabs/ignite/automation/common/SeleneseTestCase.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -237,7 +238,18 @@ public class SeleneseTestCase {
 	}
 
 	protected List<LogEntry> getJsConsoleErrors() {
-		if (driver instanceof FirefoxDriver) return driver.manage().logs().get(LogType.BROWSER).getAll();
-		throw new UnsupportedOperationException("JS console errors retrieval available for firefox driver only");
+		logger.info("Checking console errors");
+		if (driver instanceof ChromeDriver) {
+			List<LogEntry> logs = driver.manage().logs().get(LogType.BROWSER).getAll();
+			if (!logs.isEmpty()) {
+				for (int i = 0; i < logs.size(); i++) {
+					logger.error("Console error: " + logs.get(i));
+				}
+			}else logger.info("Console is empty");
+			return logs;
+		}
+		else logger.error("JS console errors retrieval available for chrome driver only");
+		return null;
+		/*throw new UnsupportedOperationException("JS console errors retrieval available for firefox driver only");*/
 	}
 }
