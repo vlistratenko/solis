e8010448056f4324154da50451113ec795778709
kirillgolovinn <you@example.com>
2018-03-30 12:30:44 +0300

    IG-13120 Create script for Activity Form Summary

--- src/main/java/com/salsalabs/ignite/automation/suites/api/devApi/GetEventAttendeesSummaryTest.java
+++ src/main/java/com/salsalabs/ignite/automation/suites/api/devApi/GetEventAttendeesSummaryTest.java
@@ -21,51 +21,54 @@ import com.salsalabs.ignite.automation.apiautomation.models.devApiModels.metrics
 import com.salsalabs.ignite.automation.apiautomation.tests.CommonTest;
 
 public class GetEventAttendeesSummaryTest extends CommonTest {
- private MetricsResponse metricsBeforeExecution;
-	 
-	 @BeforeTest
-	    public void getTestData() throws IOException {
-	        loadExpectedResultObj(
-	                new HashMap<String, Class<? extends ExpectedResult>>()
-	                {{
-	                    put(TEST_DATA_PATH_ATTENDEE_SUMMARY_RESPONSE + "get_event_attendee_summary.json", GetEventAttendeeResponse.class);
+	private MetricsResponse metricsBeforeExecution;
 
-	                }}
-	        );
-	    }
-	 
-	  @Parameters({"ENV"})
-	    @Test(priority = 0)
-	    public void getMetricsBeforeExecution(String env) throws IOException {
-	        ResponseEntity<MetricsResponse> response =
-	                restClient.exchange(env + Config.Endpoints.DEVAPIMETRICS, HttpMethod.GET, buildRequest(null), MetricsResponse.class);
-	        metricsBeforeExecution = response.getBody();
-	        Assert.assertTrue(response.getStatusCode().equals(HttpStatus.OK));
-	        logger.info(Thread.currentThread().getStackTrace()[1].getMethodName() + " success");
-	    }
-	  
-	 
-	   @Parameters({"ENV" , "UUID"})
-	    @Test(priority = 1)
-	    public void getEventAttendeeSummary(String env, String uuid) throws IOException {
-	        ResponseEntity<GetEventAttendeeResponse> response =
-	             restClient.exchange(env + Endpoints.ACTIVITYCORESUMMARY+ uuid+"/summary/attendees", HttpMethod.GET, buildRequest(null), GetEventAttendeeResponse.class);
-	        GetEventAttendeeResponse expectedResultObj = ((GetEventAttendeeResponse) getExpectedResult("get_event_attendee_summary.json"));
-	        Assert.assertEquals( response.getBody().getPayload(), expectedResultObj.getPayload());
-	        Assert.assertTrue(response.getStatusCode().equals(HttpStatus.OK));
-	     
-	    }
-	   
-	   @Parameters({"ENV"})
-	    @Test(priority = 2)
-	    public void getMetricsAfterExecution(String env) throws IOException {
-	        ResponseEntity<MetricsResponse> response =
-	                restClient.exchange(env + Endpoints.DEVAPIMETRICS, HttpMethod.GET, buildRequest(null), MetricsResponse.class);
-	        metricsBeforeExecution.getPayload().setEventAttendeesList(metricsBeforeExecution.getPayload().getEventAttendeesList()+1);
-	        metricsBeforeExecution.getPayload().setTotalApiCalls(metricsBeforeExecution.getPayload().getTotalApiCalls()+1);
-	        
-	        Assert.assertEquals(response.getBody().getPayload(), metricsBeforeExecution.getPayload());
-	        logger.info(Thread.currentThread().getStackTrace()[1].getMethodName() + " success");
-	    }
+	@BeforeTest
+	public void getTestData() throws IOException {
+		loadExpectedResultObj(new HashMap<String, Class<? extends ExpectedResult>>() {
+			{
+				put(TEST_DATA_PATH_ATTENDEE_SUMMARY_RESPONSE + "get_event_attendee_summary.json",
+						GetEventAttendeeResponse.class);
+
+			}
+		});
+	}
+
+	@Parameters({ "ENV" })
+	@Test(priority = 0)
+	public void getMetricsBeforeExecution(String env) throws IOException {
+		ResponseEntity<MetricsResponse> response = restClient.exchange(env + Config.Endpoints.DEVAPIMETRICS,
+				HttpMethod.GET, buildRequest(null), MetricsResponse.class);
+		metricsBeforeExecution = response.getBody();
+		Assert.assertTrue(response.getStatusCode().equals(HttpStatus.OK));
+		logger.info(Thread.currentThread().getStackTrace()[1].getMethodName() + " success");
+	}
+
+	@Parameters({ "ENV", "UUID" })
+	@Test(priority = 1)
+	public void getEventAttendeeSummary(String env, String uuid) throws IOException {
+		ResponseEntity<GetEventAttendeeResponse> response = restClient.exchange(
+				env + Endpoints.ACTIVITYCORESUMMARY + uuid + "/summary/attendees", HttpMethod.GET, buildRequest(null),
+				GetEventAttendeeResponse.class);
+		GetEventAttendeeResponse expectedResultObj = ((GetEventAttendeeResponse) getExpectedResult(
+				"get_event_attendee_summary.json"));
+		Assert.assertEquals(response.getBody().getPayload(), expectedResultObj.getPayload());
+		Assert.assertTrue(response.getStatusCode().equals(HttpStatus.OK));
+
+	}
+
+	@Parameters({ "ENV" })
+	@Test(priority = 2)
+	public void getMetricsAfterExecution(String env) throws IOException {
+		ResponseEntity<MetricsResponse> response = restClient.exchange(env + Endpoints.DEVAPIMETRICS, HttpMethod.GET,
+				buildRequest(null), MetricsResponse.class);
+		metricsBeforeExecution.getPayload()
+				.setEventAttendeesList(metricsBeforeExecution.getPayload().getEventAttendeesList() + 1);
+		metricsBeforeExecution.getPayload()
+				.setTotalApiCalls(metricsBeforeExecution.getPayload().getTotalApiCalls() + 1);
+
+		Assert.assertEquals(response.getBody().getPayload(), metricsBeforeExecution.getPayload());
+		logger.info(Thread.currentThread().getStackTrace()[1].getMethodName() + " success");
+	}
 
 }
