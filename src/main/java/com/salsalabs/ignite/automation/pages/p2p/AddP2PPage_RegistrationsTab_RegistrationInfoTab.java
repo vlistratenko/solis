c29375a91a6a88ba945c8cb185e4c31f30d81b63
Oleg Lauta <olauta@salsalabs.com>
2018-06-21 12:21:13 +0300

    merge conflicts fixes

--- src/main/java/com/salsalabs/ignite/automation/pages/p2p/AddP2PPage_RegistrationsTab_RegistrationInfoTab.java
+++ src/main/java/com/salsalabs/ignite/automation/pages/p2p/AddP2PPage_RegistrationsTab_RegistrationInfoTab.java
@@ -12,13 +12,14 @@ public class AddP2PPage_RegistrationsTab_RegistrationInfoTab extends AddP2PPage_
 	Button breadCrumbsLink = new ButtonImpl("//li[contains(@class, 'active')]/a[contains(.,'Registration Info')]", "Registration Info tab");
 	TextBox registrationNameField = new TextBoxImpl("//input[@id='ticket_name']", "Registration Name field");
 	BooleanRadiobutton isFundraiserOptionRadioButton = new BooleanRadiobuttonImpl("//input[@id='fundraising_option_yes']", "//input[@id='fundraising_option_no']", "Is fundraising option availalbe");
-	BooleanRadiobutton isFundraiserRadioButton = new BooleanRadiobuttonImpl("//input[@id='require_fundraiser_yes']", "//input[@id='require_fundraiser_no']", "Is fundraising account required"); 
+	BooleanRadiobutton isFundraiserRadioButton = new BooleanRadiobuttonImpl("//input[@id='require_fundraiser_yes']", "//input[@id='require_fundraiser_no']", "Is fundraising account required");
 	TextBox ticketPriceField = new TextBoxImpl("//input[@id='ticket_price']", "Ticket price field");
 	TextBox ticketDeductibleAmountField = new TextBoxImpl("//input[@id='ticket_deductible_amount']", "Ticket deductible amount field");
 	Button saveTicketButton = new ButtonImpl("//input[@value='Save']", "Save ticket button");
 
 	public AddP2PPage_RegistrationsTab_InformationCollectedTab fillRegistrationStepAndGoNext(
 			String registrationName,
+			boolean isFundraiserOption,
 			boolean isFundraiser,
 			String ticketPrice,
 			String ticketDeductible) {
@@ -26,6 +27,8 @@ public class AddP2PPage_RegistrationsTab_RegistrationInfoTab extends AddP2PPage_
 		switchToFrame(gzFrame.getPath());
 		registrationNameField.waitElement();
 		registrationNameField.type(registrationName);
+		isFundraiserOptionRadioButton.select(isFundraiserOption);
+		isFundraiserRadioButton.waitElement();
 		isFundraiserRadioButton.select(isFundraiser);
 		ticketPriceField.type(ticketPrice);
 		ticketDeductibleAmountField.type(ticketDeductible);
@@ -33,7 +36,7 @@ public class AddP2PPage_RegistrationsTab_RegistrationInfoTab extends AddP2PPage_
 		switchDefaultContent();
 		return new AddP2PPage_RegistrationsTab_InformationCollectedTab();
 	}
-	
+
 	public  AddP2PPage_RegistrationsTab_InformationCollectedTab clickOnSaveButton() {
 		saveTicketButton.scrollIntoView();
 		saveTicketButton.click();
